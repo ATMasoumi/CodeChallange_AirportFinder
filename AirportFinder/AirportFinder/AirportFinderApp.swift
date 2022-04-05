e58d62f595cbc9d27d1060af8395eaef6407438c
amin torabi masoumi <Torabi.dsd@gmail.com>
2022-04-05 23:25:17 +0430

    adding Xcode project

---
+++ AirportFinder/AirportFinder/AirportFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirportFinderApp.swift
+//  AirportFinder
+//
+//  Created by Amin on 1/16/1401 AP.
+//
+
+import SwiftUI
+
+@main
+struct AirportFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AirportFinder/AirportFinder/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AirportFinder
+//
+//  Created by Amin on 1/16/1401 AP.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
